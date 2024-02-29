43b583c584a025d9c890b314d144b2c2eb1240c7
Joseph-Oh123 <96635506+Joseph-Oh123@users.noreply.github.com>
2024-02-28 23:06:40 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import { faker } from '@faker-js/faker';
 import { BoxPlotController, BoxAndWiskers } from '@sgratzl/chartjs-chart-boxplot';
 import Chart from 'chart.js/auto'
 
-const generateRandomNumber = () => faker.number.int({ min: 1, max: 20 });
+const generateRandomNumber = () => faker.number.int({ min: 1, max: 35 });
 Chart.register(BoxPlotController, BoxAndWiskers);
 const createArray = (length, callback) => Array.from({ length }, callback);
 
-const generateDataset = (length) => {
+const generateDataset = (length, minX, maxX, minY, maxY) => {
     return createArray(length, () => ({
-        x: faker.number.int({ min: 0, max: 100 }),
-        y: faker.number.int({ min: 0, max: 100 }),
+        x: faker.number.int({ min: minX, max: maxX }),
+        y: faker.number.int({ min: minY, max: maxY }),
     }));
 };
 
 //ALABAMA STUFF ********************************************************************
 const barDataAlabama = {
-    labels: ['Caucasian', 'African American', 'Asian'],
+    labels: ['Caucasian', 'African American'],
     datasets: [
         {
             label: 'Ethnicity of Alabama House Representatives',
-            data: createArray(3, () => generateRandomNumber()),
-            backgroundColor: ['blue', 'green', 'red'],
+            data: createArray(2, () => generateRandomNumber()),
+            backgroundColor: ['blue', 'green'],
             borderWidth: 1,
         },
     ],
@@ -35,7 +35,7 @@ const barOptionsAlabama = {
     scales: {
         x: {
             indexAxis: 'Ethnicity',
-            labels: ['Caucasian', 'African American', 'Asian'],
+            labels: ['Caucasian', 'African American'],
         },
         y: {
             indexAxis: 'Number of Representatives',
@@ -47,12 +47,12 @@ const barOptionsAlabama = {
 const scatterDataAlabama = {
     datasets: [
         {
-            label: 'rep1 in general election',
-            data: generateDataset(500),
+            label: 'Katie Britt',
+            data: generateDataset(1700),
             backgroundColor: 'rgba(255, 99, 132, 1)',
         },
         {
-            label: 'rep2 in general election',
+            label: 'Will Boyd',
             data: generateDataset(500),
             backgroundColor: 'rgba(75, 192, 192, 1)',
         },
@@ -64,17 +64,17 @@ const scatterOptionsAlabama = {
         x: {
             title: {
                 display: true,
-                text: 'Percent African American', // Text for the x-axis label
+                text: 'Percent African American',
             },
         },
         y: {
             beginAtZero: true,
             title: {
                 display: true,
-                text: 'Vote Share', // Text for the x-axis label
+                text: 'Vote Share',
             },
             ticks: {
-                stepSize: 20, // Set the step size for the x-axis
+                stepSize: 20,
             },
         },
     },
@@ -111,13 +111,13 @@ const barOptionsDelaware = {
 const scatterDataDelaware = {
     datasets: [
         {
-            label: 'rep1 in general election',
-            data: generateDataset(500),
+            label: 'Gerald Hocker',
+            data: generateDataset(150),
             backgroundColor: 'rgba(255, 99, 132, 1)',
         },
         {
-            label: 'rep2 in general election',
-            data: generateDataset(500),
+            label: 'David Sokola',
+            data: generateDataset(250),
             backgroundColor: 'rgba(75, 192, 192, 1)',
         },
     ],
@@ -128,17 +128,17 @@ const scatterOptionsDelaware = {
         x: {
             title: {
                 display: true,
-                text: 'Percent African American', // Text for the x-axis label
+                text: 'Percent African American',
             },
         },
         y: {
             beginAtZero: true,
             title: {
                 display: true,
-                text: 'Vote Share', // Text for the x-axis label
+                text: 'Vote Share',
             },
             ticks: {
-                stepSize: 20, // Set the step size for the x-axis
+                stepSize: 20,
             },
         },
     },
@@ -155,13 +155,13 @@ function App() {
             
             const boxplotData = {
                 labels: ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16',
-                    '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33'],
+                    '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35'],
                 datasets: [
                     {
                         x: 8,
                         y: 16,
                         z: 2,
-                        label: 'Dataset 1',
+                        label: 'Alabama Districts',
                         backgroundColor: 'rgba(255,0,0,0.5)',
                         borderColor: 'red',
                         borderWidth: 1,
@@ -202,6 +202,8 @@ function App() {
                             randomFloats(100, .58, .68),
                             randomFloats(100, .62, .68),
                             randomFloats(40, .64, .70),
+                            randomFloats(100, .68, .74),
+                            randomFloats(40, .7, .76),
                         ],
                     },
                 ],
@@ -248,24 +250,21 @@ function App() {
 
             const boxplotData = {
                 labels: ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16',
-                    '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33'],
+                    '17','18','19','20','21'],
                 datasets: [
                     {
                         x: 8,
                         y: 16,
                         z: 2,
-                        label: 'Dataset 1',
-                        backgroundColor: 'rgba(255,0,0,0.5)',
-                        borderColor: 'red',
+                        label: 'Delaware Districts',
+                        backgroundColor: 'rgba(3,138,255,0.5)',
+                        borderColor: 'blue',
                         borderWidth: 1,
                         outlierColor: '#999999',
                         padding: 10,
                         itemRadius: 0,
                         data: [
                             randomFloats(100, 0.05, .1),
-                            randomFloats(100, 0.06, .12),
-                            randomFloats(100, .06, .14),
-                            randomFloats(100, .07, .16),
                             randomFloats(40, .07, .18),
                             randomFloats(100, .07, .2),
                             randomFloats(100, .09, .22),
@@ -274,26 +273,18 @@ function App() {
                             randomFloats(100, .14, .28),
                             randomFloats(100, .16, .30),
                             randomFloats(40, .18, .32),
-                            randomFloats(100, .22, .34),
-                            randomFloats(100, .25, .38),
-                            randomFloats(100, .34, .40),
                             randomFloats(100, .30, .42),
                             randomFloats(100, .35, .44),
                             randomFloats(100, .40, .46),
                             randomFloats(40, .42, .48),
                             randomFloats(100, .45, .49),
-                            randomFloats(100, .45, .50),
-                            randomFloats(100, .45, .52),
-                            randomFloats(100, .48, .53),
                             randomFloats(100, .49, .54),
                             randomFloats(100, .5, .55),
                             randomFloats(40, .42, .58),
-                            randomFloats(100, .48, .60),
                             randomFloats(100, .55, .60),
                             randomFloats(100, .55, .62),
                             randomFloats(100, .58, .64),
                             randomFloats(100, .58, .68),
-                            randomFloats(100, .62, .68),
                             randomFloats(40, .64, .70),
                         ],
                     },

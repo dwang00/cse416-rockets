841e179cfe92bd200ccc82703d2d6afed2c9eb31
JACHAOLEESBU <123850749+JACHAOLEESBU@users.noreply.github.com>
2024-02-29 22:22:17 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,15 +8,31 @@ import GetData from "./get_data";
 import React, { useState, useEffect } from 'react';
 
 
-const generateRandomNumber = () => faker.number.int({ min: 1, max: 35 });
+const generateRandomNumber_large = () => faker.number.int({ min: 50, max: 75 });
+const generateRandomNumber_med = () => faker.number.int({ min: 15, max: 35 });
+const generateRandomNumber_small = () => faker.number.int({ min: 1, max: 10 });
 Chart.register(BoxPlotController, BoxAndWiskers);
-const createArray = (length, callback) => Array.from({ length }, callback);
+//const createArray = (length, callback) => Array.from({ length }, callback);
 
-const generateDataset = (length, minX, maxX, minY, maxY) => {
-    return createArray(length, () => ({
+const generateDataset = (length, minX, maxX, minY, maxY, slope, intercept, noise) => {
+    /*return createArray(length, () => ({
         x: faker.number.int({ min: minX, max: maxX }),
         y: faker.number.int({ min: minY, max: maxY }),
-    }));
+    }));*/
+    return Array.from({ length }, () => {
+        const x = faker.number.float({min: minX, max: maxX});
+        let y = slope * x + intercept + (Math.random() * 2 - 1) * noise; // Adding noise for randomness
+        if(y > 1){
+            y = 1;
+        }else if(y < -1){
+            y = -1;
+        }
+        const rand = faker.number.float({min: 0, max: 1})
+        if(rand > .75){
+            y = faker.number.float({min: minX, max: maxX});
+        }
+        return { x, y };
+    });
 };
 
 //ALABAMA STUFF ********************************************************************
@@ -25,7 +41,7 @@ const barDataAlabama = {
     datasets: [
         {
             label: 'Ethnicity of Alabama House Representatives',
-            data: createArray(2, () => generateRandomNumber()),
+            data: [generateRandomNumber_large(),generateRandomNumber_med()],
             backgroundColor: ['blue', 'green'],
             borderWidth: 1,
         },
@@ -50,12 +66,14 @@ const scatterDataAlabama = {
     datasets: [
         {
             label: 'Katie Britt',
-            data: generateDataset(1700),
+            //const generateDataset = (length, minX, maxX, minY, maxY, slope, intercept, noise) => {
+
+            data: generateDataset(400,0, 1, 0, 1, .48, .19, .12),
             backgroundColor: 'rgba(255, 99, 132, 1)',
         },
         {
             label: 'Will Boyd',
-            data: generateDataset(500),
+            data: generateDataset(168,0, 1, 0, 1, -.18, .73, .15),
             backgroundColor: 'rgba(75, 192, 192, 1)',
         },
     ],
@@ -88,7 +106,7 @@ const barDataDelaware = {
     datasets: [
         {
             label: 'Ethnicity of Delaware House Representatives',
-            data: createArray(3, () => generateRandomNumber()),
+            data: [generateRandomNumber_large(),generateRandomNumber_med(),generateRandomNumber_small()],
             backgroundColor: ['blue', 'green', 'red'],
             borderWidth: 1,
         },
@@ -114,12 +132,13 @@ const scatterDataDelaware = {
     datasets: [
         {
             label: 'Gerald Hocker',
-            data: generateDataset(150),
+
+            data: generateDataset(220,0, 1, 0, 1, .3, .39, .12),
             backgroundColor: 'rgba(255, 99, 132, 1)',
         },
         {
             label: 'David Sokola',
-            data: generateDataset(250),
+            data: generateDataset(80,0, 1, 0, 1, -.2, .63, .18),
             backgroundColor: 'rgba(75, 192, 192, 1)',
         },
     ],

89396e2956a9487dab94174168e76f7c3929b19e
danieloh1213 <daniel.oh@stonybrook.edu>
2024-05-10 14:43:10 -0400

    small fix to the updated csv

--- python_stuff/cse416_seawulf-main/DE_2022updated.py
+++ python_stuff/cse416_seawulf-main/DE_2022updated.py
@@ -8,6 +8,6 @@ columns_to_keep = ['geoid20', 'total_reg', 'eth1_aa', 'eth1_eur', 'g20221108_vot
 df = df[columns_to_keep]
 
 # Save the updated dataset to a new CSV file
-df.to_csv('DE_2022_updated_stats', index=False)
+df.to_csv('DE_2022_stats_updated.csv', index=False)
 
 print("Dataset has been updated and saved to 'updated_dataset.csv'")

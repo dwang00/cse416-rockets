c73457044c34d1923e113988a67889ab90472c2e
Daniel Oh <daniel.oh@stonybrook.edu>
2024-05-11 11:35:14 -0400

    added ecological inference endpoints

---
+++ java_stuff/src/main/java/com/example/java_stuff/ecoInf/EcoInfRepository.java
@@ -0,0 +1,12 @@
+package com.example.java_stuff.ecoInf;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface EcoInfRepository extends MongoRepository<EcoInfData, String>{
+    List<EcoInfData> findAll();
+    List<EcoInfData> findByElection(String election);
+}

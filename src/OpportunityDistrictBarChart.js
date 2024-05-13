7265c0d1c5ba307dde9ae4c196de0b099a4643c8
dwang00 <25120924+dwang00@users.noreply.github.com>
2024-05-13 11:16:13 -0400

    opp bar graph tweaks

--- src/OpportunityDistrictBarChart.js
+++ src/OpportunityDistrictBarChart.js
@@ -19,13 +19,14 @@ function OpportunityDistrictBarChart({ state, race, ensemble, threshold}) {
             .catch(error => {
                 console.error("Error fetching data:", error)
             })
-    }, [state, race, ensemble, threshold])
-    console.log(oppDistrictsData)
+    }, [state, race, ensemble, threshold]);
+    console.log("opp dists", race);
+    console.log(oppDistrictsData);
     const oppData = {
         labels: Array.from({ length: 41 }, (_, i) => i + 1), // Assuming you have 40 data points
         datasets: [
             {
-                label: race,
+                label: "African American",
                 backgroundColor: 'rgba(75,192,192,0.2)',
                 borderColor: 'rgba(75,192,192,1)',
                 borderWidth: 1,
@@ -41,7 +42,7 @@ function OpportunityDistrictBarChart({ state, race, ensemble, threshold}) {
             x: {
                 title: {
                     display: true,
-                    text: "Opportunity Districts"
+                    text: "Number of Opportunity Districts"
                 }
             },
             y: {
@@ -55,7 +56,12 @@ function OpportunityDistrictBarChart({ state, race, ensemble, threshold}) {
             }
         }
     };
-    return <Bar data={oppData} options={optionsOppData} style = {{height: "330px"}}/>;
+    console.log("ok")
+    return (
+        <div style={{height: "100%"}}>
+            <Bar data={oppData} options={optionsOppData} style = {{height: "100%"}}/>
+        </div>
+    );
 
 }
 export default OpportunityDistrictBarChart;
